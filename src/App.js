342395519285f385315789cc305110881f310d67
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-24 12:10:18 +0200

    Ajout de l'arborescence des dossiers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div>
-      <h1>COUCOU</h1>
+      <Banner />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div>
+      <h1>La maison des plantes</h1>
+    </div>
+  );
+}
+
+export default Banner;
