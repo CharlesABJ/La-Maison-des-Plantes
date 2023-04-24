189c376f398f32d9941a6d7c3c240a1a45b3349c
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-24 12:28:40 +0200

    Creation et stylisation de la banniere de l'application

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import "../styles/Banner.css";
 function Banner() {
   return (
-    <div>
+    <div className="banner">
+      <img className="logo" src="./images/logo.png" alt="logo" />
       <h1>La maison des plantes</h1>
     </div>
   );
