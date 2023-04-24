18bf0dbde0c8ef89e141def3f48ffa1de9210a4e
CharlesABJ <biadjeu.charles@orange.fr>
2023-04-24 16:23:04 +0200

    les categories et les plantes s'affichent dynamamiquement sous forme de listes

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { plantList } from "../datas/plantList";
 function ShoppingList() {
+  const plantCategories = plantList.reduce(
+    (acc, plant) =>
+      acc.includes(plant.category) ? acc : acc.concat(plant.category),
+    []
+  );
   return (
-    <ul>
-      {plantList.map((plant) => (
-        <li key={plant.id}>{plant.name}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {plantCategories.map((cat, index) => (
+          <li key={`${cat}-${index}`}>{cat}</li>
+        ))}
+      </ul>
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>{plant.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
